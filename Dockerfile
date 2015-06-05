362a840c71907151a38b9c0323c0b4776daaa17c
CoreOS Admin <core@ip-172-31-24-70.us-west-1.compute.internal>
2015-06-05 00:07:21 +0000

    Do not send to librato but instead send to statsd

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM node:0.12.0
+FROM node:0.12
 MAINTAINER Meteorhacks
 
 COPY ./package.json /app/package.json
 RUN cd /app && npm install
 COPY . /app
-
-EXPOSE 11011
 
 CMD ["node", "/app/index.js"]
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 var env = require('envalid');
-var librato = require('librato-node');
+var StatsD = require('node-statsd');
 var allcontainers = require('docker-allcontainers');
 var dockerstats = require('docker-stats');
+var _ = require('lodash');
 var through = require('through2');
 
 env.validate(process.env, {
-  LIBRATO_EMAIL: {required: true},
-  LIBRATO_TOKEN: {required: true},
+  STATSD_HOST: {recommended: true},
+  STATSD_PORT: {recommended: true},
+  STATSD_PREFIX: {recommended: true},
 });
 
-librato.configure({
-  email: env.get('LIBRATO_EMAIL'),
-  token: env.get('LIBRATO_TOKEN'),
+var statsdClient =  new StatsD({
+  host: env.get('STATSD_HOST', '127.0.0.1'),
+  port: env.get('STATSD_PORT', 8125),
+  prefix: env.get('STATSD_PREFIX', 'docker.'),
 });
 
-// start librato
-librato.start();
+/*statsdClient = {
+  gauge: function(key, value){
+    console.log(key, value);
+  }
+}*/
 
 var stats = dockerstats({
   docker: null,
-  events: allcontainers({docker:null})
+  events: allcontainers({
+    preheat: true,
+    docker:null})
 });
 
 stats.pipe(through.obj(update));
@@ -27,15 +35,34 @@ stats.pipe(through.obj(update));
 function update(chunk, enc, callback) {
   var name = chunk.name;
   var info = {
-    pcpu: chunk.stats.cpu_stats.cpu_usage.cpu_percent,
-    memory: chunk.stats.memory_stats.stats.total_rss,
+    cpu: {
+      cpu_percent: chunk.stats.cpu_stats.cpu_usage.percent,
+    },
+    memory: {
+      usage: chunk.stats.memory_stats.usage,
+      max_usage: chunk.stats.memory_stats.max_usage,
+      total_rss: chunk.stats.memory_stats.stats.total_rss,
+      total_swap: chunk.stats.memory_stats.stats.total_swap,
+      total_pgpgin: chunk.stats.memory_stats.stats.total_pgpgin,
+      total_pgpgout: chunk.stats.memory_stats.stats.total_pgpgout,
+      total_pgfault: chunk.stats.memory_stats.stats.total_pgfault
+    }
   };
 
+  //console.log(chunk.stats);
+  
   updateContainer(name, info)
   callback();
 }
 
 function updateContainer (name, info) {
-  librato.measure('dstats-'+name+'-pcpu', info.pcpu);
-  librato.measure('dstats-'+name+'-memory', info.memory);
+  _.each(info, function(section, key){
+    if (!_.isObject(section)) {
+      statsdClient.gauge(name + '.' + key, section);
+    } else {
+      _.each(section, function(value, metric){
+        statsdClient.gauge(name + '.' + key + '.' + metric, value);
+      });
+    }
+  });
 }
